03fd8bab35e506870d4c2a5de0a292534d097027
Teng Yun <davenkin@126.com>
2025-12-21 15:31:45 +0800

    init

---
+++ src/main/java/com/company/andy/andy_springboot4_template/AndySpringboot4TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.company.andy.andy_springboot4_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndySpringboot4TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndySpringboot4TemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/andy/andy_springboot4_template/AndySpringboot4TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.andy.andy_springboot4_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndySpringboot4TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
