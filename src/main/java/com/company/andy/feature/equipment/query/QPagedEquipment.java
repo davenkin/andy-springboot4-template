be501dfc18901d26481dd4812cd22dd46a4f8d2f
Teng Yun <davenkin@126.com>
2025-12-24 09:50:42 +0800

    update

--- src/main/java/com/company/andy/feature/equipment/query/QPagedEquipment.java
+++ src/main/java/com/company/andy/feature/equipment/query/QPagedEquipment.java
@@ -1,14 +1,22 @@
 package com.company.andy.feature.equipment.query;
 
 import com.company.andy.feature.equipment.domain.EquipmentStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.Instant;
 
+@Schema(description = "Paged equipment")
 public record QPagedEquipment(
+        @Schema(description = "ID of the equipment")
         String id,
+        @Schema(description = "Org ID of the equipment")
         String orgId,
+        @Schema(description = "Name of the equipment")
         String name,
+        @Schema(description = "Status of the equipment")
         EquipmentStatus status,
+        @Schema(description = "Create time of the equipment")
         Instant createdAt,
+        @Schema(description = "Creator of the equipment")
         String createdBy) {
 }
