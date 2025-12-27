34cd165180f1b1256460e9f47718dc8de08a5d18
Teng Yun <davenkin@126.com>
2025-12-27 18:15:48 +0800

    update

--- src/test/java/com/company/andy/IntegrationTest.java
+++ src/test/java/com/company/andy/IntegrationTest.java
@@ -5,8 +5,6 @@ import com.company.andy.common.event.DomainEventType;
 import com.company.andy.common.event.consume.EventConsumer;
 import com.company.andy.common.event.publish.PublishingDomainEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,9 +14,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.client.RestTestClient;
-import redis.embedded.RedisServer;
-
-import java.io.IOException;
 
 import static com.company.andy.common.util.CommonUtils.mongoConcatFields;
 import static com.company.andy.common.util.CommonUtils.requireNonBlank;
@@ -35,7 +30,6 @@ import static org.springframework.data.mongodb.core.query.Query.query;
 @AutoConfigureRestTestClient
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public abstract class IntegrationTest {
-    private static RedisServer redisServer;
 
     @Autowired
     protected MongoTemplate mongoTemplate;
@@ -51,22 +45,6 @@ public abstract class IntegrationTest {
 
     @Autowired
     protected RestTestClient restTestClient;
-
-    @BeforeAll
-    public static void beforeAll() {
-        try {
-            // RedisServer is only used by application-it.yaml, but not application-it-local.yaml as it uses real Redis server from local machine
-            redisServer = new RedisServer(6126);
-            redisServer.start();
-        } catch (IOException e) {
-            log.error("Failed to start embedded Redis server.", e);
-        }
-    }
-
-    @AfterAll
-    public static void afterAll() throws IOException {
-        redisServer.stop();
-    }
 
     protected <T extends DomainEvent> T latestEventFor(String arId, DomainEventType type, Class<T> eventClass) {
         requireNonBlank(arId, "arId must not be blank.");
---
+++ src/test/java/com/company/andy/support/EmbeddedRedisServer.java
@@ -0,0 +1,36 @@
+package com.company.andy.support;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@Profile("it")
+public class EmbeddedRedisServer {
+    private static RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedisServer() {
+        try {
+            redisServer = new RedisServer(6126);
+            redisServer.start();
+        } catch (IOException e) {
+            log.warn("Failed to start embedded Redis server.", e);
+        }
+    }
+
+    @PreDestroy
+    public void stopRedisServer() {
+        try {
+            redisServer.stop();
+        } catch (IOException e) {
+            log.warn("Failed to stop embedded Redis server", e);
+        }
+    }
+}
