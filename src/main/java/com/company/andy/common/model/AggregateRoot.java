8192e7790ac9886805270b5ff2d9795811f2f27c
Teng Yun <davenkin@126.com>
2026-05-10 23:23:52 +0800

    update

--- src/main/java/com/company/andy/common/model/AggregateRoot.java
+++ src/main/java/com/company/andy/common/model/AggregateRoot.java
@@ -92,4 +92,9 @@ public abstract class AggregateRoot {
         this.modifiedBy = modifiedBy;
         this.modifiedAt = Instant.now();
     }
+
+    protected void markModified(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+        this.modifiedAt = Instant.now();
+    }
 }
