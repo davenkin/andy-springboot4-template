087649b2d5b5b807b61ff5535c8c862060ad9ec0
davenkin <davenkin@126.com>
2026-05-11 09:49:10 +0800

    update

--- src/main/java/com/company/andy/common/tracing/ActorMdcPropagatingTaskDecorator.java
+++ src/main/java/com/company/andy/common/tracing/ActorMdcPropagatingTaskDecorator.java
@@ -1,30 +1,29 @@
 package com.company.andy.common.tracing;
 
+import static java.util.stream.Collectors.toMap;
+
+import static com.company.andy.common.tracing.ActorMdcSupport.ACTOR_MDC_KEYS;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 import org.jspecify.annotations.NullMarked;
 import org.slf4j.MDC;
 import org.springframework.core.task.TaskDecorator;
 
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static com.company.andy.common.tracing.ActorMdcSupport.ACTOR_MDC_KEYS;
-
 @NullMarked
 public class ActorMdcPropagatingTaskDecorator implements TaskDecorator {
 
-    @Override
-    public Runnable decorate(Runnable runnable) {
-        Map<String, String> mdcValues = ACTOR_MDC_KEYS.stream()
-                .filter(key -> MDC.get(key) != null)
-                .collect(Collectors.toMap(key -> key, MDC::get));
-
-        return () -> {
-            mdcValues.forEach(MDC::put);
-            try {
-                runnable.run();
-            } finally {
-                ACTOR_MDC_KEYS.forEach(MDC::remove);
-            }
-        };
-    }
+  @Override
+  public Runnable decorate(Runnable runnable) {
+    return () -> {
+      ACTOR_MDC_KEYS.stream()
+          .filter(key -> isNotBlank(MDC.get(key)))
+          .collect(toMap(key -> key, MDC::get))
+          .forEach(MDC::put);
+      try {
+        runnable.run();
+      } finally {
+        ACTOR_MDC_KEYS.forEach(MDC::remove);
+      }
+    };
+  }
 }
