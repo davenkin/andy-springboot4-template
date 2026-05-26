febb39c4bc7775046ecc8f4191ab5a320a90c3c2
Teng Yun <davenkin@126.com>
2026-05-27 06:27:03 +0800

    updat

--- src/main/java/com/company/andy/common/configuration/CommonConfiguration.java
+++ src/main/java/com/company/andy/common/configuration/CommonConfiguration.java
@@ -8,8 +8,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.resilience.annotation.EnableResilientMethods;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.ALWAYS;
 import static com.fasterxml.jackson.annotation.PropertyAccessor.ALL;
+import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
 import static tools.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static tools.jackson.databind.MapperFeature.REQUIRE_SETTERS_FOR_GETTERS;
 import static tools.jackson.databind.cfg.DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS;
@@ -22,7 +24,7 @@ public class CommonConfiguration {
     @Bean
     public JsonMapperBuilderCustomizer jsonMapperBuilderCustomizer() {
         return builder -> builder
-                .changeDefaultVisibility(it -> it.withVisibility(ALL, ANY))
+                .changeDefaultVisibility(it -> it.withVisibility(ALL, NONE).withVisibility(FIELD, ANY))
                 .changeDefaultPropertyInclusion(it -> it.withValueInclusion(ALWAYS))
                 .enable(REQUIRE_SETTERS_FOR_GETTERS)
                 .disable(FAIL_ON_UNKNOWN_PROPERTIES)
